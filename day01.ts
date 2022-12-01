355ee4572a2b1f4c254a13dfc9ab93c88e05c35f
Amelia Lim Chorng Uan <amelia@Amelias-MBP.fritz.box>
2022-12-01 21:06:52 +0100

    day 01

---
+++ day01.ts
@@ -0,0 +1,17 @@
+const parse = (input: string): Array<Array<number>> => {
+  return input.split('\n\n').map((x) => {
+    return x.split('\n').map(y => {
+      return parseInt(y, 10)
+    })
+  })
+}
+
+const top3Sum = (input: Array<Array<number>>): number => {
+  const sums = input.map(x => x.reduce((acc, curr) => acc + curr, 0))
+
+  const top1 = Math.max(...sums);
+  const top2 = Math.max(...sums.filter(x => x !== top1))
+  const top3 = Math.max(...sums.filter(x => x !== top1 && x!== top2))
+
+  return top1 + top2 + top3
+}
